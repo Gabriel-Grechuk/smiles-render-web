8e05a1d6f263aa67fa9692d6cec8cdb5313c57d9
Gabe <gabrielgrechuk@outlook.com>
2024-11-09 17:29:22 -0300

    #2 - Add /render/<smiles> route

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, send_file
+from converter import convert_smiles
 
 app = Flask(__name__)
 
@@ -13,10 +14,23 @@ def index():
     return """
     <html>
         <head>
-            <title>Hey there</title>
+            <title>SMILES render</title>
         </head>
         <body>
-            <h1>Hey there!</h1>
+            <h1>Coming soon!</h1>
         </body>
     </html>
     """
+
+
+@app.route("/render/<string:smiles>")
+def render_smiles(smiles: str):
+    try:
+        format = request.args.get("format") or "png"
+        image = convert_smiles(smiles, format.lower())
+
+        return send_file(image, f"image/{format}")
+
+    except Exception as err:
+        print(err)
+        return f"Could not convert smiles: {err}"
