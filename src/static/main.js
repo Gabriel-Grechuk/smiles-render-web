c737910e3362650c6185fc03f68ee16f98c5da93
Gabe <gabrielgrechuk@outlook.com>
2024-11-17 03:08:16 -0300

    #4 - Add Initial render UI

--- src/static/main.js
+++ src/static/main.js
@@ -1,23 +1,51 @@
-function fetchImage() {
-  const inputString = document.getElementById('inputString').value;
-  const imageContainer = document.getElementById('image-container');
-  imageContainer.innerHTML = 'Loading...';
+function renderImages() {
+  const inputStrings =
+      document.getElementById('smiles-input-string').value.split('\n');
+  const imagesContainer = document.getElementById('rendered-images');
 
-  fetch(`/render/${inputString}`, {
-    method : 'GET',
+  imagesContainer.innerHTML = '';
+
+  for (const smiles of inputStrings) {
+    const node = document.createElement('div');
+    node.className = 'smiles-image';
+
+    const image = document.createElement('img');
+    image.setAttribute('src', `/render/${smiles}`);
+    image.setAttribute('alt', smiles);
+
+    const desc = document.createElement('p');
+    desc.innerHTML = smiles;
+
+    node.appendChild(image);
+    node.appendChild(desc);
+    imagesContainer.appendChild(node);
+  }
+}
+
+function downloadImages() {
+  const inputStrings =
+      document.getElementById('smiles-input-string').value.split('\n');
+
+  fetch('/render', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({format: 'png', smiles: inputStrings})
   })
       .then(response => {
-        if (!response.ok)
-          throw new Error('Network response was not ok');
+        if (!response.ok) {
+          throw new Error('Could not get response from network');
+        }
         return response.blob();
       })
       .then(blob => {
-        const imageUrl = URL.createObjectURL(blob);
-        imageContainer.innerHTML =
-            `<img src="${imageUrl}" alt="Returned Image">`;
+        const link = document.createElement('a');
+        const url = URL.createObjectURL(blob);
+        link.href = url;
+        link.download = 'smiles.zip';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        URL.revokeObjectURL(url);
       })
-      .catch(error => {
-        console.error('Error:', error);
-        imageContainer.innerHTML = 'Failed to load image';
-      });
+      .catch(error => { console.error('Error downloading the file:', error); });
 }
