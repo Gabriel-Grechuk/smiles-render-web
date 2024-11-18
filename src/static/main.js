552755cf0174ddf15b39fc0184b84d477d8b4fc9
Gabe <gabrielgrechuk@outlook.com>
2024-11-18 13:43:59 -0300

    #12 - Format with prettierd

--- src/static/main.js
+++ src/static/main.js
@@ -1,19 +1,20 @@
 function renderImages() {
-  const inputStrings =
-      document.getElementById('smiles-input-string').value.split('\n');
-  const imagesContainer = document.getElementById('rendered-images');
+  const inputStrings = document
+    .getElementById("smiles-input-string")
+    .value.split("\n");
+  const imagesContainer = document.getElementById("rendered-images");
 
-  imagesContainer.innerHTML = '';
+  imagesContainer.innerHTML = "";
 
   for (const smiles of inputStrings) {
-    const node = document.createElement('div');
-    node.className = 'smiles-image';
+    const node = document.createElement("div");
+    node.className = "smiles-image";
 
-    const image = document.createElement('img');
-    image.setAttribute('src', `/render/${smiles}`);
-    image.setAttribute('alt', smiles);
+    const image = document.createElement("img");
+    image.setAttribute("src", `/render/${smiles}`);
+    image.setAttribute("alt", smiles);
 
-    const desc = document.createElement('p');
+    const desc = document.createElement("p");
     desc.innerHTML = smiles;
 
     node.appendChild(image);
@@ -23,29 +24,32 @@ function renderImages() {
 }
 
 function downloadImages() {
-  const inputStrings =
-      document.getElementById('smiles-input-string').value.split('\n');
+  const inputStrings = document
+    .getElementById("smiles-input-string")
+    .value.split("\n");
 
-  fetch('/render', {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({format: 'png', smiles: inputStrings})
+  fetch("/render", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ format: "png", smiles: inputStrings }),
   })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Could not get response from network');
-        }
-        return response.blob();
-      })
-      .then(blob => {
-        const link = document.createElement('a');
-        const url = URL.createObjectURL(blob);
-        link.href = url;
-        link.download = 'smiles.zip';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-        URL.revokeObjectURL(url);
-      })
-      .catch(error => { console.error('Error downloading the file:', error); });
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Could not get response from network");
+      }
+      return response.blob();
+    })
+    .then((blob) => {
+      const link = document.createElement("a");
+      const url = URL.createObjectURL(blob);
+      link.href = url;
+      link.download = "smiles.zip";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      URL.revokeObjectURL(url);
+    })
+    .catch((error) => {
+      console.error("Error downloading the file:", error);
+    });
 }
