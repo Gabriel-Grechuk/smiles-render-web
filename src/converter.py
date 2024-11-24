96e5e94687f8b6bcfe0a2967ffc36c4f023dc29d
Gabe <gabrielgrechuk@outlook.com>
2024-11-24 18:39:13 -0300

    #15 - Only one implementation for convert_many_smiles_and_zip()
    This one have named images

--- src/converter.py
+++ src/converter.py
@@ -3,8 +3,6 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 import zipfile
 import re
-from tools import micro_hash
-from os import path
 
 
 supported_formats = [
@@ -30,7 +28,7 @@ supported_formats = [
 
 
 def sanitize_file_name(name: str) -> str:
-    return re.sub(r"[^a-zA-Z0-9]", "", name).lower() + "_" + micro_hash(name)
+    return re.sub(r"[^a-zA-Z0-9]", "", name).lower()
 
 
 def valid_format(format: str) -> bool:
@@ -50,41 +48,27 @@ def convert_smiles(smiles: str, format: str) -> BytesIO:
     return bin_image
 
 
-def convert_many_smiles_and_zip(smiles: list[tuple[str, str]]) -> BytesIO:
+def convert_many_smiles_and_zip(smiles: list[tuple[str, str, str]]) -> BytesIO:
     zip_file = BytesIO()
 
-    with zipfile.ZipFile(zip_file, "w") as zip:
-        for smile, format in smiles:
-            image = convert_smiles(smile, format)
-            zip.writestr(
-                f"{sanitize_file_name(smile)}.{format.lower()}", image.getvalue()
-            )
-    zip_file.seek(0)
-
-    return zip_file
-
-
-def convert_many_named_smiles_and_zip(smiles: list[tuple[str, str]]) -> BytesIO:
-    zip_file = BytesIO()
-
-    already_assigned_names: list[str] = []
+    used_names_cout: dict[str, int] = {}
 
     with zipfile.ZipFile(zip_file, "w") as zip:
-        for smile, name in smiles:
-            name = name.lower()
-
-            original_format = path.splitext(name)[1].replace(".", "")
-            format = original_format if valid_format(original_format) else "png"
-
-            name = name.replace(original_format, format)
-
-            if name in already_assigned_names:
-                name = "duplicated-" + name
-
-            already_assigned_names.append(name)
-
+        for smile, name, format in smiles:
+            format = format if valid_format(format) else "png"
             image = convert_smiles(smile, format)
-            zip.writestr(f"{name}", image.getvalue())
+
+            if not name:
+                name = sanitize_file_name(smile)
+
+            if used_names_cout[name]:
+                used_names_cout[name] += 1
+                name = f"{name} {used_names_cout[name]}"
+
+            used_names_cout[name] = 1
+
+            zip.writestr(f"{name}.{format.lower()}", image.getvalue())
+
     zip_file.seek(0)
 
     return zip_file
