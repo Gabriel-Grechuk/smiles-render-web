7168f48ce55c9bc591c01d2f9e9211e7bab7b203
Gabe <gabrielgrechuk@outlook.com>
2024-12-21 13:15:35 -0300

    #28 - Add Footer component

---
+++ src/frontend/components/Footer.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const pageFooterStyle = {
+  width: '100%',
+  marginTop: '10px',
+  padding: '10px',
+  display: 'flex',
+  justifyContent: 'center',
+  backgroundColor: '#000',
+  color: '#fff',
+  alignItems: 'center',
+};
+
+function Footer() {
+  return (
+    <footer style={pageFooterStyle}>
+      <span
+        style={{
+          margin: '0px 5px 0px 5px',
+        }}
+      >
+        {' '}
+        Made with
+        <i
+          className="bi bi-heart-fill heart"
+          style={{ color: 'red', fontSize: '12px' }}
+        ></i>
+        by Gabe
+      </span>
+      <div style={{ margin: 'margin: 0px 5px 0 5px' }}>
+        <a
+          href="https://github.com/Gabriel-Grechuk"
+          target="_blank"
+          style={{ margin: '2px' }}
+        >
+          <i className="bi bi-github" style={{ fontSize: '18px' }}></i>
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
