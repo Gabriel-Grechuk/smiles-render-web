87310aed9b39ae5d0fd90b24045e6529755dfa74
Gabe <gabrielgrechuk@outlook.com>
2024-12-21 13:16:10 -0300

    #28 - Add Section component

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import Section from './components/Section';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          width: '95%',
+          minHeight: '100vh',
+        }}
+      >
+        <Section title="Direct input" content={<p>Hoy there</p>} />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
