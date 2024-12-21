e3b3d40e5e80b18505bb3870a6bde608bc5ae724
Gabe <gabrielgrechuk@outlook.com>
2024-12-21 02:26:12 -0300

    #28 - Add prettier and eslint configs to the project

--- frontend-build.ts
+++ frontend-build.ts
@@ -1,19 +1,20 @@
-import type { BuildConfig } from "bun";
+import type { BuildConfig } from 'bun';
 
 const output = await Bun.build({
-  entrypoints: ["./src/frontend/index.tsx"],
-  outdir: "./src/static/build",
+  entrypoints: ['./src/frontend/index.tsx'],
+  outdir: './src/static/build',
   minify: true,
-  target: "browser",
-  format: "esm",
-  env: "inline",
-  drop: ["console"],
+  target: 'browser',
+  format: 'esm',
+  env: 'inline',
+  drop: ['console'],
 } as BuildConfig);
 
 if (!output.success) {
-  console.log("[ ERR ] - Seems that we have some problems at the build...");
-  for (const log of output.logs) console.log("Error: ", log);
+  console.log('[ ERR ] - Seems that we have some problems at the build...');
+  for (const log of output.logs) console.log('Error: ', log);
   process.exit(1);
 } else {
-  console.log("[ OK ] - Success!");
+  console.log('[ OK ] - Success!');
+  for (const log of output.outputs) console.log('Error: ', log);
 }
