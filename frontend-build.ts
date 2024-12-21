3a9bf6657d01e7957e12d69e037599e8f891a0cc
Gabe <gabrielgrechuk@outlook.com>
2024-12-21 00:41:37 -0300

    #28 - Add frontend-build.ts

---
+++ frontend-build.ts
@@ -0,0 +1,19 @@
+import type { BuildConfig } from "bun";
+
+const output = await Bun.build({
+  entrypoints: ["./src/frontend/index.tsx"],
+  outdir: "./src/static/build",
+  minify: true,
+  target: "browser",
+  format: "esm",
+  env: "inline",
+  drop: ["console"],
+} as BuildConfig);
+
+if (!output.success) {
+  console.log("[ ERR ] - Seems that we have some problems at the build...");
+  for (const log of output.logs) console.log("Error: ", log);
+  process.exit(1);
+} else {
+  console.log("[ OK ] - Success!");
+}
